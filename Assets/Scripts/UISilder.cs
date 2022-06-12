9bded31b951bd6e8d8a4f5a594f5c3aa34bf6e91
LHEALP <57874136+LHEALP@users.noreply.github.com>
2022-06-12 15:16:49 +0900

    Create UISlider.cs

---
+++ Assets/Scripts/UISilder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISilder : UIObject
+{
+    public Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener(OnValue);
+    }
+
+    public void OnValue(float value)
+    {
+        slider.value = value;
+        UIController.Instance.find.Invoke(Name);
+    }
+}
