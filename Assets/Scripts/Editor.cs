1be07923d19482b9937b71687ab5d9d608a3ec54
LHEALP <57874136+LHEALP@users.noreply.github.com>
2022-06-12 15:18:53 +0900

    Create Editor.cs

---
+++ Assets/Scripts/Editor.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Editor : MonoBehaviour
+{
+    static Editor instance;
+    public static Editor Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    UISilder slider = null;
+    UIButton musicController = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Up()
+    {
+
+    }
+
+    public void Down()
+    {
+
+    }
+
+    void Update()
+    {
+        float value = 1 / AudioManager.Instance.Length * AudioManager.Instance.progressTime;
+        if (float.IsNaN(value))
+            return;
+
+        if (slider == null)
+            slider = UIController.Instance.FindUI("UI_E_ProgressBar").uiObject as UISilder;
+        else
+            slider.OnValue(value);
+    }
+
+    public void Play(UIObject uiObject)
+    {
+        if (musicController == null)
+            musicController = uiObject as UIButton;
+        else
+        {
+            switch (AudioManager.Instance.state)
+            {
+                case AudioManager.State.Playing:
+                    {
+                        AudioManager.Instance.Pause();
+                        musicController.SetText(">");
+                    }
+                    break;
+                case AudioManager.State.Paused:
+                    {
+                        AudioManager.Instance.UnPause();
+                        musicController.SetText("||");
+                    }
+                    break;
+                case AudioManager.State.Unpaused:
+                    {
+                        AudioManager.Instance.Pause();
+                        musicController.SetText(">");
+                    }
+                    break;
+                case AudioManager.State.Stop:
+                    {
+                        AudioManager.Instance.Play();
+                        musicController.SetText("||");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void Stop(UIObject uiObject)
+    {
+        AudioManager.Instance.Stop();
+        if (musicController != null)
+            musicController.SetText(">");
+    }
+
+    public void Progress(UIObject uiObject)
+    {
+        if (slider != null)
+        {
+            float time = AudioManager.Instance.Length * slider.slider.value;
+            AudioManager.Instance.progressTime = time;
+        }
+    }
+}
