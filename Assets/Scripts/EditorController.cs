8bae0fe6884490896b5aaaa6ffbbbe961de02ad6
LHEALP <57874136+LHEALP@users.noreply.github.com>
2022-07-12 01:13:58 +0900

    Update EditorController.cs

--- Assets/Scripts/EditorController.cs
+++ Assets/Scripts/EditorController.cs
@@ -18,6 +18,9 @@ public class EditorController : MonoBehaviour
     float scrollValue;
     Coroutine coCtrl;
 
+    Camera cam;
+    Vector3 worldPos;
+
     InputManager inputManager;
     AudioManager audioManager;
     Editor editor;
@@ -32,6 +35,8 @@ public class EditorController : MonoBehaviour
 
     void Start()
     {
+        cam = Camera.main;
+
         inputManager = FindObjectOfType<InputManager>();
         audioManager = AudioManager.Instance;
         editor = Editor.Instance;
@@ -41,17 +46,50 @@ public class EditorController : MonoBehaviour
     {
         // 그리드에 레이쏴서 위치 알아내야함
         // 현재 스냅에 따라, 스냅될 위치 알아내야함
-        Debug.Log(inputManager.mousePos);
+        //Debug.Log(inputManager.mousePos);
+        Vector3 mousePos = inputManager.mousePos;
+        mousePos.z = -cam.transform.position.z;
+        worldPos = cam.ScreenToWorldPoint(mousePos);
+        //int layerMask = (1 << LayerMask.NameToLayer("Grid")) + (1 << LayerMask.NameToLayer("Note"));
+
+        Debug.DrawRay(worldPos, cam.transform.forward * 2, Color.red, 0.2f);
+        RaycastHit2D hit = Physics2D.Raycast(worldPos, cam.transform.forward, 2f);
+        if (hit)
+        {
+            int line = int.Parse(hit.transform.name.Split('_')[1]);
+            int index = hit.transform.parent.GetComponent<GridObject>().index;
+            if (worldPos.x < -1f && worldPos.x > -2f)
+            {
+                Debug.Log($"0번 레인 : {index}번 그리드 : {line} 비트");
+            }
+            else if (worldPos.x < 0.5f && worldPos.x > -1f)
+            {
+                Debug.Log($"1번 레인 : {index}번 그리드 : {line} 비트");
+            }
+            else if (worldPos.x < 1f && worldPos.x > 0.5f)
+            {
+                Debug.Log($"2번 레인 : {index}번 그리드 : {line} 비트");
+            }
+            else if (worldPos.x < 1.5f && worldPos.x > 1f)
+            {
+                Debug.Log($"3번 레인 : {index}번 그리드 : {line} 비트");
+            }
+        }
     }
 
-    // 스페이스 - 재생/일시정지( Space - Play/Puase )
+    /// <summary>
+    /// 스페이스 - 재생/일시정지( Space - Play/Puase )
+    /// </summary>
     public void Space()
     {
         Editor.Instance.Play();
     }
 
-    // 마우스좌클릭 - 노트 배치 ( Mouse leftBtn - Dispose note )
-    // 마우스우클릭 - 노트 삭제 ( Mouse rightBtn - Cancel note )
+    /// <summary>
+    /// 좌클릭 - 노트 배치 ( Mouse leftBtn - Dispose note )
+    /// 우클릭 - 노트 삭제 ( Mouse rightBtn - Cancel note )
+    /// </summary>
+    /// <param name="btnName"></param>
     public void MouseBtn(string btnName)
     {
         if (btnName == "leftButton")
@@ -64,13 +102,18 @@ public class EditorController : MonoBehaviour
         }
     }
 
-    // 마우스휠 - 음악 및 그리드 위치 이동 ( Mouse wheel - Move music and grids pos )
+    /// <summary>
+    /// 마우스휠 - 음악 및 그리드 위치 이동 ( Mouse wheel - Move music and grids pos )
+    /// </summary>
+    /// <param name="value"></param>
     public void Scroll(float value)
     {
         scrollValue = value;
     }
 
-    // 컨트롤 + 마우스휠 - 그리드 스냅 변경 ( Ctrl + Mouse wheel - Change snap of grids )
+    /// <summary>
+    /// 컨트롤 + 마우스휠 - 그리드 스냅 변경 ( Ctrl + Mouse wheel - Change snap of grids )
+    /// </summary>
     public void Ctrl()
     {
         if (coCtrl != null)
@@ -88,15 +131,15 @@ public class EditorController : MonoBehaviour
             {
                 // 스냅업
                 Editor.Instance.Snap /= 2;
+                GridSnapListener.Invoke(Editor.Instance.Snap);
             }
             else if (scrollValue < 0)
             {
                 // 스냅다운
                 Editor.Instance.Snap *= 2;
+                GridSnapListener.Invoke(Editor.Instance.Snap);
             }
             scrollValue = 0;
-
-            GridSnapListener.Invoke(Editor.Instance.Snap);
 
             yield return null;
         }
@@ -104,7 +147,10 @@ public class EditorController : MonoBehaviour
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(100, 100, 100, 100), scrollValue.ToString());
-        GUI.Label(new Rect(100, 200, 100, 100), Editor.Instance.Snap.ToString());
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 36;
+        GUI.Label(new Rect(100, 100, 100, 100), "Mouse Pos : " + inputManager.mousePos.ToString(), style);
+        GUI.Label(new Rect(100, 200, 100, 100), "ScreenToWorld : " + worldPos.ToString(), style);
+        GUI.Label(new Rect(100, 300, 100, 100), "CurrentBar : " + Editor.Instance.currentBar.ToString(), style);
     }
 }
